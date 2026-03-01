05186d9a9f2e982ebda17adcb6870a0da2567244
Francis Catacutan <fml.catacutan@gmail.com>
2026-03-01 22:46:02 +0800

    feature(global role) - add function to handle global role for user controller - add exception handling for global role - update update user details and password

--- backend/src/main/java/com/example/task_manager/user/AdminUserController.java
+++ backend/src/main/java/com/example/task_manager/user/AdminUserController.java
@@ -3,15 +3,13 @@ package com.example.task_manager.user;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.task_manager.config.security.CustomUserPrincipal;
-import com.example.task_manager.exception.api.UnauthorizedException;
 import com.example.task_manager.user.dto.UpdateUserRoleRequest;
 
 import jakarta.validation.Valid;
@@ -36,16 +34,9 @@ public class AdminUserController {
   public ResponseEntity<Void> updateUserRole(
       @PathVariable UUID id,
       @Valid @RequestBody UpdateUserRoleRequest request,
-      @AuthenticationPrincipal CustomUserPrincipal currentUser) {
+      Authentication authentication) {
 
-    if (currentUser == null) {
-      throw new UnauthorizedException("Authentication is Required");
-    }
-
-    userService.updateUserRole(
-        id,
-        request.role(),
-        currentUser.getId());
+    userService.updateUserRole(id, request, authentication.getName());
 
     return ResponseEntity.noContent().build();
   }
